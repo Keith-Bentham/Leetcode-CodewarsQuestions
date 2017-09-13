f5cb460dbc389ccde2aff866ace8d6415a0faa20
Keith Bentham <keithbentham92@gmail.com>
2017-09-13 14:14:45 +0100

    no message

---
+++ src/TwoSum.java
@@ -0,0 +1,22 @@
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by Keith Bentham
+ */
+class TwoSum {
+    public int[] findSumInArray(int[] nums, int target) {
+        Map<Integer, Integer> foundValues = new HashMap<Integer, Integer>();
+        for (int i = 0; i < nums.length; i++) {
+            int match = target - nums[i];
+            if (foundValues.containsKey(match)) {
+                return new int[]{
+                        foundValues.get(match), i
+                };
+            }
+            foundValues.put(nums[i], i);
+        }
+        throw new IllegalArgumentException("No solution found");
+    }
+}
+
