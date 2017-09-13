80dd0ac44c184115b22d226491ced94b4e83cda6
Keith Bentham <keithbentham92@gmail.com>
2017-09-13 20:19:05 +0100

    no message

---
+++ src/GetSum.java
@@ -0,0 +1,19 @@
+/**
+ * Created by Keith Bentham
+ * Get the sum of two integers without the use of + or -
+ */
+
+public class GetSum {
+    public int getSum(int a, int b) {
+        return b == 0 ? a : getSum(a ^ b, (a & b) << 1);
+    }
+    /**
+     * if b is equal to 0 if its true return a recursive call to a to the power of b
+     * otherwise return (a exclusive OR b) and left shift 1
+     */
+
+    public static void main(String[] args) {
+        GetSum gs = new GetSum();
+        System.out.println(gs.getSum(43, 2));
+    }
+}
