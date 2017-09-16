4a7d62599b96f1080c09a80a4d9c52489aeacfd0
Keith Bentham <keithbentham92@gmail.com>
2017-09-16 22:01:05 +0100

    no message

--- src/ValidAnagram.java
+++ src/ValidAnagram.java
@@ -4,11 +4,20 @@
 public class ValidAnagram {
     public boolean isAnagram(String s, String t) {
         int[] alphabet = new int[26];
-        for (int i = 0; i < s.length(); i++) alphabet[s.charAt(i) - 'a']++;
-        for (int i = 0; i < t.length(); i++) alphabet[t.charAt(i) - 'a']--;
-        for (int i : alphabet) if (i != 0) return false;
+        for (int i = 0; i < s.length(); i++){
+            alphabet[s.charAt(i) - 'a']++;
+        }
+        for (int i = 0; i < t.length(); i++){
+            alphabet[t.charAt(i) - 'a']--;
+        }
+        for (int i : alphabet){
+            if (i != 0){
+                return false;
+            }
+        }
         return true;
     }
+
     public static void main(String[] args) {
         ValidAnagram va = new ValidAnagram();
         System.out.println("is \"anagram\" and \"nagaram\" valid anagrams of each other: " + va.isAnagram("anagram", "nagaram"));
