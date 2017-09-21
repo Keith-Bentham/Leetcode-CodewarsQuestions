836d4536cfe4920ac821ce5a4b96e73d4379bb6c
Keith Bentham <keithbentham92@gmail.com>
2017-09-21 20:57:02 +0100

    no message

---
+++ src/ValidBinarySearchTree.java
@@ -0,0 +1,55 @@
+import java.util.Stack;
+
+/**
+ * Created by Keith Bentham
+ */
+public class ValidBinarySearchTree {
+
+    private static class NodeBounds {
+
+        BinaryTreeNode node;
+        int lowerBound;
+        int upperBound;
+
+        NodeBounds(BinaryTreeNode node, int lowerBound, int upperBound) {
+            this.node = node;
+            this.lowerBound = lowerBound;
+            this.upperBound = upperBound;
+        }
+    }
+
+    public static boolean isBinarySearchTree(BinaryTreeNode root) {
+
+        // start at the root, with an arbitrarily low lower bound
+        // and an arbitrarily high upper bound
+        Stack<NodeBounds> nodeAndBoundsStack = new Stack<>();
+        nodeAndBoundsStack.push(new NodeBounds(root, Integer.MIN_VALUE, Integer.MAX_VALUE));
+
+        // depth-first traversal
+        while (!nodeAndBoundsStack.empty()) {
+            NodeBounds nb = nodeAndBoundsStack.pop();
+            BinaryTreeNode node = nb.node;
+            int lowerBound = nb.lowerBound;
+            int upperBound = nb.upperBound;
+
+            // if this node is invalid, we return false right away
+            if (node.value <= lowerBound || node.value >= upperBound) {
+                return false;
+            }
+
+            if (node.left != null) {
+                // this node must be less than the current node
+                nodeAndBoundsStack.push(new NodeBounds(node.left, lowerBound, node.value));
+            }
+            if (node.right != null) {
+                // this node must be greater than the current node
+                nodeAndBoundsStack.push(new NodeBounds(node.right, node.value, upperBound));
+            }
+        }
+
+        // if none of the nodes were invalid, return true
+        // (at this point we have checked all nodes)
+        return true;
+    }
+
+}
