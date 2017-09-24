f4d0a257405f0b826749b39a4daecad9759a75d9
Keith Bentham <keithbentham92@gmail.com>
2017-09-24 19:08:12 +0100

    no message

--- README.md
+++ README.md
@@ -35,3 +35,4 @@ Kth Smallest Element in a BST   https://leetcode.com/problems/kth-smallest-eleme
 Binary Tree Inorder Traversal   https://leetcode.com/problems/binary-tree-inorder-traversal/description/
 Permutations                https://leetcode.com/problems/permutations/description/
 First Unique Character in a String  https://leetcode.com/problems/first-unique-character-in-a-string/description/
+Odd Even Linked List        
---
+++ src/OddEvenLinkedList.java
@@ -0,0 +1,18 @@
+/**
+ * Created by Keith Bentham
+ */
+public class OddEvenLinkedList {
+    public ListNode oddEvenList(ListNode head) {
+        if (head != null) {
+            ListNode odd = head, even = head.next, evenHead = even;
+            while (even != null && even.next != null) {
+                odd.next = odd.next.next;
+                even.next = even.next.next;
+                odd = odd.next;
+                even = even.next;
+            }
+            odd.next = evenHead;
+        }
+        return head;
+    }
+}
