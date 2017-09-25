376d46b4340e05d3ae47aee739168d6499d3a4b9
Keith Bentham <keithbentham92@gmail.com>
2017-09-25 15:14:01 +0100

    no message

--- src/ContainsDuplicate.java
+++ src/ContainsDuplicate.java
@@ -7,15 +7,27 @@ import java.util.Set;
 public class ContainsDuplicate {
     public boolean containsDuplicate(int[] nums) {
         Set<Integer> arraySet = new HashSet<>();
-        for(int i : nums)
-            if(!arraySet.add(i))
+        for (int i : nums)
+            if (!arraySet.add(i))
                 return true;
         return false;
     }
 
+    public static int findSingle(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        for (int item : nums) {
+            if (!set.remove(item)) {
+                set.add(item);
+            }
+        }
+        assert set.size() == 1;
+        return set.iterator().next();
+    }
+
     public static void main(String[] args) {
-        int[] arr = {1, 2, 3, 4, 5, 5};
+        int[] arr = {1, 1, 1, 2, 2, 3, 3 , 4, 4, 5, 5};
         ContainsDuplicate cd = new ContainsDuplicate();
-        System.out.println("Does the array contain duplicates?: " + cd.containsDuplicate(arr) );
+        System.out.println("Does the array contain duplicates?: " + cd.containsDuplicate(arr));
+        System.out.println("Does the array contain duplicates?: " + cd.findSingle(arr));
     }
 }
--- src/GetSum.java
+++ src/GetSum.java
@@ -7,13 +7,35 @@ public class GetSum {
     public int getSum(int a, int b) {
         return b == 0 ? a : getSum(a ^ b, (a & b) << 1);
     }
+
+    public int getSum2(int a, int b) {
+        if (a == 0) return b;
+        if (b == 0) return a;
+
+        while (b != 0) {
+            int carry = a & b;
+            a = a ^ b;
+            b = carry << 1;
+        }
+
+        return a;
+    }
+
     /**
-     * if b is equal to 0 return true, just a as no addition is needed if its true
-     * return a recursive call if its false to a to the power of b otherwise return (a exclusive OR b) and left shift 1
+     * For this, problem, for example, we have a = 1, b = 3,
+     In bit representation, a = 0001, b = 0011,
+     First, we can use "and"("&") operation between a and b to find a carry.
+     carry = a & b, then carry = 0001
+     Second, we can use "xor" ("^") operation between a and b to find the different bit, and assign it to a,
+     Then, we shift carry one position left and assign it to b, b = 0010.
+     Iterate until there is no carry (or b == 0)
+     * if b is equal to 0 return a as there is nothing to add
+     * return a recursive call if its false to a to the power of b return (a exclusive OR b) and left shift 1
      */
 
     public static void main(String[] args) {
         GetSum gs = new GetSum();
         System.out.println(gs.getSum(43, 2));
+        System.out.println(gs.getSum2(1, 3));
     }
 }
