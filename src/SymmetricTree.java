091b3d4431397094c76c1eeaafa08370c3b6cfea
Keith Bentham <keithbentham92@gmail.com>
2017-09-26 21:33:21 +0100

    no message

---
+++ src/SymmetricTree.java
@@ -0,0 +1,29 @@
+/**
+ * Created by Keith Bentham
+ */
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+}
+
+public class SymmetricTree {
+    public boolean isSymmetric(TreeNode root) {
+        return helper(root, root);
+    }
+
+    private boolean helper(TreeNode node1, TreeNode node2) {
+        if (node1 == null && node2 == null) {
+            return true;
+        }
+        if (node1 == null || node2 == null || node1.val != node2.val) {
+            return false;
+        }
+
+        return helper(node1.left, node2.right) && helper(node1.right, node2.left);
+    }
+}
