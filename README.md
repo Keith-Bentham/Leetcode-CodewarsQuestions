7a2599a68fd30be41f44a3520eaba77dd6e841cd
Keith Bentham <keithbentham92@gmail.com>
2017-09-28 20:09:47 +0100

    no message

--- README.md
+++ README.md
@@ -43,4 +43,5 @@ Symmetric Tree              https://leetcode.com/problems/symmetric-tree/descrip
 Climbing Stairs             https://leetcode.com/problems/climbing-stairs/description/
 Subsets                     https://leetcode.com/problems/subsets/description/
 Keyboard Row                https://leetcode.com/problems/keyboard-row/description/
-Reverse Words in a String III   https://leetcode.com/problems/reverse-words-in-a-string-iii/discuss/
+Reverse Words in a String III   https://leetcode.com/problems/reverse-words-in-a-string-iii/description/
+Construct the Rectangle     https://leetcode.com/problems/construct-the-rectangle/description/
---
+++ src/ConstructRectangle.java
@@ -0,0 +1,25 @@
+import java.util.Arrays;
+
+/**
+ * Created by Keith Bentham
+ */
+public class ConstructRectangle {
+    public int[] constructRectangle(int area) {
+        int[] result = new int[2];
+        if (area == 0) {
+            return result;
+        }
+        int a = (int) Math.sqrt(area);
+        while (area % a != 0) {
+            a--;
+        }
+        int b = area / a;
+        result[0] = b;
+        result[1] = a;
+        return result;
+    }
+    public static void main(String[] args) {
+        ConstructRectangle cr = new ConstructRectangle();
+        System.out.println("In op?: " + Arrays.toString(cr.constructRectangle(4)));
+    }
+}
