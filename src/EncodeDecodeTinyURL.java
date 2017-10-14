bc48ab7659dd1457a8caef9f222bdc2953e7dd7e
Keith Bentham <keithbentham92@gmail.com>
2017-10-14 21:42:48 +0100

    no message

--- src/EncodeDecodeTinyURL.java
+++ src/EncodeDecodeTinyURL.java
@@ -1,11 +1,12 @@
 import java.util.HashMap;
+
 /**
  * Created by Keith Bentham
  */
 public class EncodeDecodeTinyURL {
-    HashMap<String, String> map= new HashMap<String, String>();
-    // Encodes a URL to a shortened URL.
+    HashMap<String, String> map = new HashMap<String, String>();
 
+    // Encodes a URL to a shortened URL.
     public String encode(String longUrl) {
         map.put(Integer.toString(map.size()), longUrl);
         return Integer.toString(map.size() - 1);
