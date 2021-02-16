a0c33c75f8de3c9249bc05b65362ef2b660696e0
keithbentham <keithbentham92@gmail.com>
2021-02-16 11:24:28 +0000

    Update.

--- src/OddEvenLinkedList.java
+++ src/OddEvenLinkedList.java
@@ -20,3 +20,13 @@ public class OddEvenLinkedList {
         return head;
     }
 }
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ * int val;
+ * ListNode next;
+ * ListNode() {}
+ * ListNode(int val) { this.val = val; }
+ * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
