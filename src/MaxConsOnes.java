9f1e3dfccf485fc10143152f4b3a85f133722657
keithbentham <keithbentham92@gmail.com>
2021-03-15 20:09:53 +0200

    Update.

--- src/MaxConsOnes.java
+++ src/MaxConsOnes.java
@@ -1,10 +1,8 @@
 public class MaxConsOnes {
-
     public static void main(String[] args) {
         int[] nums = {1, 0, 1, 0, 2, 2, 2, 2, 2, 1};
         System.out.println("Max 1s is :" +
                 findMaxConsecutiveOnes(nums));
-
     }
 
     private static int findMaxConsecutiveOnes(int[] nums) {
@@ -20,5 +18,20 @@ public class MaxConsOnes {
             }
         }
         return Math.max(foundOne, max);
+    }
+
+    public static int findCon(int[] nums) {
+        int found1 = 0;
+        int max1 = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 1) {
+                found1++;
+            } else {
+                max1 = Math.max(found1, max1);
+                found1 = 0;
+            }
+        }
+        return Math.max(found1, max1);
     }
 }
