d310b9da8290ab95a59a27da4894759ada5dad7a
keithbentham <keithbentham92@gmail.com>
2021-04-19 17:48:08 +0800

    Update.

--- src/MaxConsOnes.java
+++ src/MaxConsOnes.java
@@ -19,4 +19,38 @@ public class MaxConsOnes {
         }
         return Math.max(foundOne, max);
     }
+
+
+    public static int maxConsecOnes(int[] arr) {
+        int maxOnes = 0;
+        int foundOne = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == 1) {
+                foundOne++;
+            } else {
+                maxOnes = Math.max(foundOne, maxOnes);
+                foundOne = 0;
+            }
+        }
+        return Math.max(foundOne, maxOnes);
+    }
+
+
+    public static int countConFives(int[] arr) {
+        int maxFive = 0;
+        int foundFive = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == 5) {
+                foundFive++;
+            } else {
+                maxFive = Math.max(maxFive, foundFive);
+                foundFive = 0;
+            }
+        }
+        return Math.max(foundFive, maxFive);
+    }
+
+
 }
